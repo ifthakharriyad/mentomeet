f496c07a0abaacd7deb38a117df9fe9771b9a63b
iftekharriyad <iftekharriyad@protonmail.com>
2020-08-26 23:12:21 +0600

    added About us, Beyond Mentorship, Why Choose Mentorship components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import './css/App.css';
+
 import WhyChooseUs from './components/WhyChooseUs'
 import Home from './components/Home'
 import CareerSolutionEveryone from "./components/CareerSolutionEveryone"
 import BlogDetailedPage from './components/BlogPostDetailed/BlogDetailedPage'
 import BlogList from './components/BlogList/BlogList'
+import About from './components/About'
+import BeyondMentorship from './components/BeyondMentorship'
+import WhyChooseMentomeet from './components/WhyChooseMentomeet'
 
 
 function App() {
@@ -15,6 +20,9 @@ function App() {
       <WhyChooseUs/> 
       <BlogDetailedPage/>
       <BlogList/>
+      <About/>
+      <BeyondMentorship/>
+      <WhyChooseMentomeet/>
     </div>
   )
 }
